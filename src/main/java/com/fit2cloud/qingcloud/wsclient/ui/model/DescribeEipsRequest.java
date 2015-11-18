021532763a0e40bd8b72d132f576648c447e552a
xyh <xiaosilent@gmail.com>
2015-11-18 15:44:39 +0800

    调整了获取公网IP的请求实体,将status参数改为List<String>可以接收多个状态参数.

--- src/main/java/com/fit2cloud/qingcloud/wsclient/ui/model/DescribeEipsRequest.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/ui/model/DescribeEipsRequest.java
@@ -3,69 +3,69 @@ package com.fit2cloud.qingcloud.wsclient.ui.model;
 import java.util.List;
 
 public class DescribeEipsRequest extends Request {
-	private List<String> eips;
-	private String instance_id;
-	private String status;
-	private String search_word;
-	private Integer offset;
-	private Integer limit;
+    private List<String> eips;
+    private String instance_id;
+    private List<String> status;
+    private String search_word;
+    private Integer offset;
+    private Integer limit;
 
-	private String zone;
+    private String zone;
 
-	public List<String> getEips() {
-		return eips;
-	}
+    public List<String> getEips() {
+        return eips;
+    }
 
-	public void setEips(List<String> eips) {
-		this.eips = eips;
-	}
+    public void setEips(List<String> eips) {
+        this.eips = eips;
+    }
 
-	public String getInstance_id() {
-		return instance_id;
-	}
+    public String getInstance_id() {
+        return instance_id;
+    }
 
-	public void setInstance_id(String instance_id) {
-		this.instance_id = instance_id;
-	}
+    public void setInstance_id(String instance_id) {
+        this.instance_id = instance_id;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public List<String> getStatus() {
+        return status;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    public void setStatus(List<String> status) {
+        this.status = status;
+    }
 
-	public String getSearch_word() {
-		return search_word;
-	}
+    public String getSearch_word() {
+        return search_word;
+    }
 
-	public void setSearch_word(String search_word) {
-		this.search_word = search_word;
-	}
+    public void setSearch_word(String search_word) {
+        this.search_word = search_word;
+    }
 
-	public Integer getOffset() {
-		return offset;
-	}
+    public Integer getOffset() {
+        return offset;
+    }
 
-	public void setOffset(Integer offset) {
-		this.offset = offset;
-	}
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
 
-	public Integer getLimit() {
-		return limit;
-	}
+    public Integer getLimit() {
+        return limit;
+    }
 
-	public void setLimit(Integer limit) {
-		this.limit = limit;
-	}
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
 
-	public String getZone() {
-		return zone;
-	}
+    public String getZone() {
+        return zone;
+    }
 
-	public void setZone(String zone) {
-		this.zone = zone;
-	}
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
 
 }
