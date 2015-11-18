6f3317c3f383f3381188986be096d75e6871b011
xyh <xiaosilent@gmail.com>
2015-11-18 13:36:49 +0800

    新增了主机的过渡状态描述实体

---
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudInstanceTransitionStatus.java
@@ -0,0 +1,16 @@
+package com.fit2cloud.qingcloud.wsclient.domain.model;
+
+
+public class QingCloudInstanceTransitionStatus {
+
+    public static final String CREATING = "creating";
+    public static final String STARTING = "starting";
+    public static final String STOPPING = "stopping";
+    public static final String RESTARTING = "restarting";
+    public static final String SUSPENDING = "suspending";
+    public static final String RESUMING = "resuming";
+    public static final String TERMINATING = "terminating";
+    public static final String RECOVERING = "recovering";
+    public static final String RESETTING = "resetting";
+
+}
